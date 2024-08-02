d271e0db136f12530807c0dad14d4bd3ec0cdfed
Surya <ssuurryyaa101@gmail.com>
2024-08-02 17:34:42 -0400

    Styles path fix

--- views/header.php
+++ views/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop and Go</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
